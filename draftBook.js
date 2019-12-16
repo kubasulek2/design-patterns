de9def0da942246d4228218294a7809586b32056
Kuba Sukowski <kubasulek2@gmail.com>
2019-12-16 17:14:05 +0100

    project initialized

--- draftBook.js
+++ draftBook.js
@@ -1,79 +1,12 @@
-class Creature {
-	constructor (name, attack, defense) {
-		this.name = name;
-		this.attack = attack;
-		this.defense = defense;
+let _age;
+class Person {
+	constructor (age) {
+		_age = age;
+		console.log(new.target.name);
 	}
-
-	toString () {
-		return `${ this.name } (${ this.attack }/${ this.defense })`;
-	}
+	
 }
+let me = new Person(22);
+let me2 = new Person(24);
 
-class CreatureModifier {
-	constructor (creature) {
-		this.creature = creature;
-		this.next = null;
-	}
-
-	add (modifier) {
-		if (this.next) this.next.add(modifier);
-		else this.next = modifier;
-	}
-
-	handle () {
-		if (this.next) this.next.handle();
-	}
-}
-
-class NoBonusesModifier extends CreatureModifier {
-	constructor (creature) {
-		super(creature);
-	}
-
-	handle () {
-		console.log('No bonuses for you!');
-	}
-}
-
-class DoubleAttackModifier extends CreatureModifier {
-	constructor (creature) {
-		super(creature);
-	}
-
-	handle () {
-		console.log(`Doubling ${ this.creature.name }'s attack`);
-		this.creature.attack *= 2;
-		super.handle();
-	}
-}
-
-class IncreaseDefenseModifier extends CreatureModifier {
-	constructor (creature) {
-		super(creature);
-	}
-
-	handle () {
-		if (this.creature.attack <= 2) {
-			console.log(`Increasing ${ this.creature.name }'s defense`);
-			this.creature.defense++;
-		}
-		super.handle();
-	}
-}
-
-let goblin = new Creature('Goblin', 1, 1);
-console.log(goblin.toString());
-
-let root = new CreatureModifier(goblin);
-
-//root.add(new NoBonusesModifier(goblin));
-
-root.add(new DoubleAttackModifier(goblin));
-//root.add(new DoubleAttackModifier(goblin));
-
-root.add(new IncreaseDefenseModifier(goblin));
-
-// eventually...
-root.handle();
-console.log(goblin.toString());
+console.log(me.age, me2.age);
