70446f78ababe91d4425d57e51953ceb22ecf7cc
Kuba Sukowski <kubasulek2@gmail.com>
2019-12-17 17:21:10 +0100

    command done

--- Behavioral/Command/command.md
+++ Behavioral/Command/command.md
@@ -4,3 +4,4 @@
 * if you need do sth extra, serialize, save on disk some action you use Command
 * command is an object which represents instruction to perform some action
 * command is actually wrapper
+* with command can handle undoing command itself or construct composite commands.
--- draftBook.js
+++ draftBook.js
@@ -1,82 +1,34 @@
-class BankAccount {
-	constructor (balance = 0) {
-		this.balance = balance;
-	}
-	deposit(amount) {
-		this.balance += amount;
-		console.log(
-			`Deposited ${ amount }, balance is now ${ this.balance }`
-		);
-	}
-
-	withdraw(amount) {
-		if (this.balance - amount >= BankAccount.overdraftLimit) {
-			this.balance -= amount;
-			console.log(
-				`Withdrew ${ amount }, balance is now ${ this.balance }`
-			);
-			return true;
-		}
-		return false;
-	}
-
-	toString() {
-		return `Balance: ${ this.balance }`;
-	}
-}
-BankAccount.overdraftLimit = -500;
-
 let Action = Object.freeze({
-	deposit: 1,
-	withdraw: 2
+	deposit: 0,
+	withdraw: 1
 });
 
-class BankAccountCommand {
-	constructor (account, action, amount) {
-		this.account = account;
+class Command {
+	constructor (action, amount) {
 		this.action = action;
 		this.amount = amount;
 		this.success = false;
 	}
-	call() {
-		switch (this.action) {
-			case Action.deposit:
-				this.account.deposit(this.amount);
-				this.success = true;
-				break;
-			case Action.withdraw:
-				this.success = this.account.withdraw(this.amount);
-				break;
-			default:
-				break;
-		}
-	}
-	undo() {
-		// return when command wasn't successful.		
-		if (!this.success) return;
-		switch (this.action) {
-			case Action.deposit:
-				this.account.withdraw(this.amount);
-				break;
-			case Action.withdraw:
-				this.account.deposit(this.amount);
-				break;
-			default:
-				break;
-
-		}
-		// extra protection - cant deposit and then undo two times
-		this.success = false;
-
-	}
 }
 
-let ba = new BankAccount(100);
+class Account {
+	constructor () {
+		this.balance = 0;
+	}
 
-let cmd = new BankAccountCommand(ba, Action.deposit, 200);
-cmd.call();
-
-cmd = new BankAccountCommand(ba, Action.withdraw, 900); // Failure over limit.
-cmd.call();
-cmd.undo(); // it's protected, doesn't return what have not succeeded to withdraw
-console.log(ba.balance);
+	process(cmd) {
+		switch (cmd.action) {
+			case Action.deposit:
+				this.balance += cmd.amount;
+				cmd.success = true;
+				break;
+			case Action.withdraw:
+				if (this.balance - cmd.amount < 0) return cmd.success = false;
+				this.balance = this.balance -= cmd.amount;
+				cmd.success = true;
+				break;
+			default:
+				break;
+		}
+	}
+}
